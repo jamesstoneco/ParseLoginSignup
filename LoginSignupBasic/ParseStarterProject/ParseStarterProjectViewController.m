01fad94702e6f575ccb8409342b2169864f295bb
James Stone <jamesmanofstone@gmail.com>
2013-02-13 08:48:49 -0800

    adding starter project without zip

---
+++ LoginSignupBasic/ParseStarterProject/ParseStarterProjectViewController.m
@@ -0,0 +1,28 @@
+#import "ParseStarterProjectViewController.h"
+#import <Parse/Parse.h>
+
+@implementation ParseStarterProjectViewController
+
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+
+#pragma mark - UIViewController
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
